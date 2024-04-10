57c8daa2267bdb3c893a3f68354cca1e2072121a
Roi Assayag <rockybb777@gmail.com>
2024-04-10 16:52:48 +0200

    fine tuning

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -22,6 +22,13 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+
+  const [players, setPlayers] = useState({
+    'X': 'Player 1',
+    'O': 'Player 2',
+  });
+
+
   const [gameTurns, setGameTurns] = useState([]);
   // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
@@ -52,7 +59,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -75,6 +82,14 @@ function App() {
   function handleRestart() {
     setGameTurns([]);
   }
+  function handlePlayerNameChange (symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      }
+    })
+  }
 
   return (
     <main>
@@ -84,11 +99,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
